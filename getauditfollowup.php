9b667fd874a029996ff9a9e853e2592542bf6ee3
RichardSurya <rjrichardparker@gmail.com>
2023-05-12 18:49:35 +0530

    popup

--- getauditfollowup.php
+++ getauditfollowup.php
@@ -3,25 +3,37 @@ include('ajaxconfig.php');
 
 if(isset($_POST["prev_checklist"])){
 	$prev_chekclist_id  = $_POST["prev_checklist"];
-    print_r($prev_chekclist_id);
+    // print_r($prev_chekclist_id);
 }
 if(isset($_POST["date"])){
 	$current_date  = $_POST["date"];
-    print_r($current_date);
+    // print_r($current_date);
 }
 
-$major_area = array();
-// $sub_area = array();
+$audit_assign_id = array();
 $assertion = array();
-$weightage = array();
+$recommendation = array();
+$attachment = array();
+$auditee_response = array();
+$action_plan = array();
+$target_date = array();
+$audit_status = array();
+$department_id = array();
+$role1 = array();
+$designation_name1 = array();
+$role2 = array();
+$designation_name2 = array();
 
-$qry=$con->query("SELECT aai.audit_assign_id, aai.assertion, aai.audit_remarks, aai.recommendation, aai.attachment, aai.auditee_response, aai.action_plan, aai.target_date, aai.audit_status
+$qry=$con->query("SELECT aai.audit_assign_id, aai.assertion, aai.audit_remarks, aai.recommendation, aai.attachment, aai.auditee_response, aai.action_plan, aai.target_date, aai.audit_status,aa.department_id,aa.role1,ds.designation_name AS designation_name1,aa.role2,dsc.designation_name AS designation_name2
 FROM audit_assign_ref aai
 LEFT JOIN audit_assign aa ON aa.audit_assign_id = aai.audit_assign_id
+LEFT JOIN designation_creation ds ON ds.designation_id = aa.role1
+LEFT JOIN designation_creation dsc ON dsc.designation_id = aa.role2
+
 WHERE aai.auditee_response_status = '1'
-  AND aa.audit_area_id = $prev_chekclist_id
-  AND aai.target_date >= DATE_ADD(CURDATE(), INTERVAL 3 DAY);");
-// SELECT aai.audit_assign_id,aai.assertion,aai.audit_remarks,aai.recommendation,aai.attachment,aai.auditee_response,aai.action_plan,aai.target_date,aai.audit_status FROM audit_assign_ref aai LEFT JOIN audit_assign aa ON aa.audit_assign_id = aai.audit_assign_id WHERE  aai.auditee_response_status='0' AND aa.audit_area_id='$prev_chekclist_id' AND aai.target_date NOT IN ('NULL');
+  AND aa.audit_area_id = 1
+  AND aai.target_date >= DATE_ADD(CURDATE(), INTERVAL 3 DAY)");
+
 while($row=$qry->fetch_assoc()){
     $audit_assign_id[] = $row['audit_assign_id'];
     $assertion[] = $row['assertion'];
@@ -31,16 +43,31 @@ while($row=$qry->fetch_assoc()){
     $auditee_response[] = $row['auditee_response'];
     $action_plan[] = $row['action_plan'];
     $target_date[]= $row['target_date'];
-    $audit_status[]= $row['audit_status'];
+    $audit_status[]= $row['audit_status']; 
+    $department_id[]= $row['department_id'];
+    $role1[]= $row['role1'];
+    $designation_name1[]= $row['designation_name1']; 
+    $role2[]= $row['role2']; 
+    $designation_name2[]= $row['designation_name2'];
     
    
 }
 
-for($i=0;$i<count($major_area); $i++){
-    $prevChecklist[$i]['major_area'] = $major_area[$i];
-    // $prevChecklist[$i]['sub_area'] = $sub_area[$i];
+for($i=0;$i<count($audit_assign_id); $i++){
+    $prevChecklist[$i]['audit_assign_id'] = $audit_assign_id[$i];
     $prevChecklist[$i]['assertion'] = $assertion[$i];
-    $prevChecklist[$i]['weightage'] = $weightage[$i];
+    $prevChecklist[$i]['audit_remarks'] = $audit_remarks[$i];
+    $prevChecklist[$i]['recommendation'] = $recommendation[$i];
+    $prevChecklist[$i]['attachment'] = $attachment[$i];
+    $prevChecklist[$i]['auditee_response'] = $auditee_response[$i];
+    $prevChecklist[$i]['action_plan'] = $action_plan[$i];
+    $prevChecklist[$i]['target_date'] = $target_date[$i];
+    $prevChecklist[$i]['audit_status'] = $audit_status[$i];
+    $prevChecklist[$i]['department_id'] = $department_id[$i];
+    $prevChecklist[$i]['role1'] = $role1[$i];
+    $prevChecklist[$i]['designation_name1'] = $designation_name1[$i];
+    $prevChecklist[$i]['role2'] = $role2[$i];
+    $prevChecklist[$i]['designation_name2'] = $designation_name2[$i];
 }
 
 echo json_encode($prevChecklist);
--- js/audit_followup.js
+++ js/audit_followup.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     //     $('#date_of_audit').attr("disabled", true);
     // }
 
-    $("#checklist").change( function(){
+    $("#prev").change( function(){
 
-        var checklist = $("#checklist").val();
-        $.ajax({
+        var checklist = $("#prev").val();
+         $.ajax({
             url: 'auditFile/ajaxGetAuditFollowup.php',
             data: {"checklist": checklist},
             cache: false,
@@ -19,17 +19,26 @@ $(document).ready(function () {
             dataType: "json",
             success: function (data) {
 
-                $("#dept").val(data['dept']);
-                $("#dept_id").val(data['dept_id']);
-                $("#role1").val(data['role1']);
-                $("#role1_id").val(data['role1_id']);
-                $("#role2").val(data['role2']);
-                $("#role2_id").val(data['role2_id']);
+
+               
+                $("#dept_id").val(data[0]['department_id']);
+                $("#role1").val(data[0]['dcrole1']);
+                $("#role1_id").val(data[0]['role1']);
+                $("#role2").val(data[0]['dcrole2']);
+                $("#role2_id").val(data[0]['role2']);
+                $("#dept").val(data['department_name']);
+
+                
+
                 
             }
         });
+        getdeptname();
     });
-
+function getdeptname(){
+    var dept_id = $("#dept_id").val();
+   
+}
     // Previous checklist dropdown open
     $('#checklist').change(function() {
         if ($(this).is(':checked')) {
@@ -52,7 +61,7 @@ $(document).ready(function () {
     //    "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" +
     //    "<td> <span class='icon-trash-2' tabindex='10' id='delete_row'></span></td></tr>";
     //     $('#moduleTable').find('tbody').append(appendTxt);
-    //     sts();
+   
     // }));
      // Add new row
      $(document).on('click','#tab_show',(function(){
@@ -87,17 +96,16 @@ $(document).ready(function () {
         "<td><input tabindex='7' type='text' class='form-control' id='rcmd' placeholder='Enter Recommendation' name='rcmd[]' ></td>"+
         "<td><input type='file' style='padding: 3px;' tabindex='8' class='form-control' id='att_file' name='file[]'></td>"+
         // "<td> <input tabindex='3' type='text' class='form-control' id='weightage' name='weightage[]'></input></td>" +
-        "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" +
+        "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Pop-Up</button></td>" +
         "<td> <span class='icon-trash-2' tabindex='10' id='delete_row'></span></td> </tr>";
         $('#moduleTable').find('tbody').html(removeData);
-        sts();
+      
     }
 
     // insert Data into Module Table
     function insertData(date_of_audit){
         $('#moduleTable').find('tbody').empty();
         var date = $('#date_of_audit').val();
-        console.log("date",date);
                     $.ajax({
                         url: 'getauditfollowup.php',
                         data: {'prev_checklist': date_of_audit ,'date':date},
@@ -105,26 +113,34 @@ $(document).ready(function () {
                         type:'post',
                         dataType: 'json',
                         success: function(data){
+                           
                         //   $('#moduleTable').find('tbody').empty();
-                            for(var a=0; a<=data.length-1; a++){
-                                markup1 = "<td><input type='text' tabindex='4' class='form-control' value="+ data[a]['major_area'] + " id='major' name='major[]' placeholder='Enter Area'></input></td>";
-                                // markup2= "<td><input type='text' tabindex='3' class='form-control'  value="+ data[a]['sub_area'] + " id='sub' name='sub[]'></input></td>";
-                                markup3 = "<td><input type='text' tabindex='5' class='form-control'  value="+ data[a]['assertion'] + " id='assertion' name='assertion[]' placeholder='Enter Assertion'></input></td>";
-                                // markup4 = "<td><input type='text' tabindex='3' class='form-control'  value="+ data[a]['weightage'] + " id='weightage' name='weightage[]'></input></td>";
-                                var dataAppend = "<tr>"+ markup1 + markup3 +
-                                "<td> <select type='text' tabindex='6' name='prevstatus[]'  id='prevstatus' class='form-control prevstatus'><option value=''>Select Status</option><option value='1'>Yes</option><option value='0'>No</option></select></td>"+
-                                "<td><textarea  id='aremarks'  class='form-control' rows='1' name='aremarks[]'  cols='35' placeholder='Enter Audit Remarks'></textarea></td>"+
-                                "<td><input tabindex='7' type='text' class='form-control' id='rcmd' name='rcmd[]' placeholder='Enter Recommendation' ></td>"+
-                                "<td><input type='file' style='padding: 3px;' tabindex='8' class='form-control' id='att_file' name='file[]'></td>"+
-                                "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" +
-                                "<td> <span class='icon-trash-2' tabindex='10' id='delete_row'></span></td> </tr>";
-                                $('#moduleTable').find('tbody').append(dataAppend);
-                                sts();
+                            for(var a=0; a<=data.length-1; a++){ 
+                                console.log("Details value",data);
+                                var dataAppend = "<tr><td><input tabindex='4' type='text' class='form-control' id='assertion'  name='assertion[]' value="+ data[a]['assertion'] +" readonly></td>"+
+                                "<td><input tabindex='7' type='text' class='form-control' id='audit_remarks' name='audit_remarks[]' value="+ data[a]['audit_remarks'] +" readonly></td>"+  
+                                "<td><a href='./uploads/audit_assign/"+ data[a]['attachment'] +"' download='filetodownload' readonly>"+ data[a]['attachment'] +"</a></td>"+
+                                "<td><input type='text' tabindex='7' class='form-control' id='auditee_response' name='auditee_response[]' value="+ data[a]['auditee_response'] + " readonly></td>"+
+                                "<td><input type='text' tabindex='7' class='form-control' id='action_plan' name='action_plan[]' value="+ data[a]['action_plan'] +" readonly></td>"+
+                                "<td><input type='text' tabindex='7' class='form-control' id='target_date' name='target_date[]' value="+ data[a]['target_date'] +" readonly></td>"+
+                                "<td><button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Open Modal</button><button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Pop-Up</button></td>"+
+                                   "<td><span class='icon-trash-2' tabindex='10' id='delete_row'></span></td> </tr>";
+                                   $('#moduleTable').find('tbody').append(dataAppend);                           
+                               
                             }
                               // $('#delete_row:last').filter(':last').attr('id', '');
                         }
                       });
     }
+    // $(document).on('click','#add_row',(function(){
+   
+    //    $('.addDesignationModal').addClass('show');
+    // }));
+    // $(document).on('click','.close',(function(){
+   
+    //     $('.addDesignationModal').removeClass('show');
+    //  }));
+    
     // SELECT aai.audit_assign_id,aai.assertion,aai.audit_remarks,aai.recommendation,aai.attachment,aai.auditee_response,aai.action_plan,aai.target_date,aai.audit_status FROM audit_assign_ref aai LEFT JOIN audit_assign aa ON aa.audit_assign_id = aai.audit_assign_id WHERE  aai.auditee_response_status='1' AND aai.target_date NOT IN ('NULL');
     // "<td> <input tabindex='7' type='text' class='form-control' id='Astatus' name='Astatus[]'></td>"+"<td><input tabindex='9' type='text' class='form-control' id='rcmd' name='rcmd[]' ></td>"+"<td><input tabindex='10' type='text' class='form-control' id='assertion' name='assertion[]' ></td>"+"<td><input tabindex='11' type='text' class='form-control' id='aremarks' name='aremarks[]' ></td>"+"<td> <button type='button' tabindex='3' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" + "<td> <span class='icon-trash-2' tabindex='' id='delete_row'></span></td></tr>"
     // //Validations
@@ -181,23 +197,6 @@ $(document).ready(function () {
 
         
     });
-    function sts() {
-            
-        $('.prevstatus').change(function() { 
-
-                var ans=$(this).val();
-                if(ans=='1'){
-                $(this).parent().next().next().children().prop('readonly', true);
-                $(this).parent().next().next().next().children().prop('readonly', true); 
-                }else{
-                    $(this).parent().next().next().children().prop('readonly', false);
-                    $(this).parent().next().next().next().children().prop('readonly', false);
-                }
-
-
-         
-        }); 
-    }
- sts();
+    
 
  
