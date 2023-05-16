6aa56ac8749a0341635310a2c523e5ad80fbf822
RichardSurya <rjrichardparker@gmail.com>
2023-05-16 18:39:43 +0530

    setup

---
+++ ajaxGoalSettingFetchDashboard.php
@@ -0,0 +1,138 @@
+<?php
+include('ajaxconfig.php');
+@session_start();
+
+if(isset($_SESSION["userid"])){
+    $userid = $_SESSION["userid"];
+}
+
+$column = array(
+    'c.company_name',
+    'department_name',
+    'designation_name',
+    'year',
+    'status'
+);
+
+$query = "SELECT gs.goal_setting_id,gs.company_name,c.company_name,gs.department,dc.department_name,gs.role,ds.designation_name,gs.year,y.year,gs.status FROM goal_setting gs 
+LEFT JOIN goal_setting_ref gsr ON gsr.goal_setting_id=gs.goal_setting_id 
+LEFT JOIN company_creation c ON c.company_id=gs.company_name 
+LEFT JOIN department_creation dc ON dc.department_id=gs.department 
+LEFT JOIN designation_creation ds ON ds.designation_id=gs.role 
+LEFT JOIN year_creation y ON y.year_id=gs.year
+ WHERE 1";
+
+if($_POST['search']!="");
+{
+    if (isset($_POST['search'])) {
+
+        if($_POST['search']=="Active")
+        {
+            $query .="and status=0 "; 
+        }
+        else if($_POST['search']=="Inactive")
+        {
+            $query .="and status=1 ";
+        }
+
+        else{	
+            $query .= "
+            
+            OR c.company_name LIKE  '%".$_POST['search']."%'
+            OR dc.department_name LIKE '%".$_POST['search']."%'
+            OR ds.designation_name LIKE '%".$_POST['search']."%'
+            OR y.year LIKE '%".$_POST['search']."%'
+            OR gs.status LIKE '%".$_POST['search']."%'";
+           
+        }
+    }
+}
+
+if (isset($_POST['order'])) {
+    $query .= ' ORDER BY ' . $column[$_POST['order']['0']['column']] . ' ' . $_POST['order']['0']['dir'] . ' ';
+} else {
+    $query .= ' ';
+}
+// print_r($query);
+$query1 = '';
+
+if ($_POST['length'] != -1) {
+    $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+}
+
+$statement = $connect->prepare($query);
+$statement->execute();
+$number_filter_row = $statement->rowCount();
+$statement = $connect->prepare($query . $query1);
+$statement->execute();
+$result = $statement->fetchAll();
+$data = array();
+// print_r($data);
+$sno = 1;
+foreach ($result as $row) {
+    $sub_array   = array();
+    
+    if($sno!="")
+    {
+        $sub_array[] = $sno;
+    }
+                        
+    $sub_array[] = $row['company_name'];
+    $sub_array[] = $row['department_name'];
+    $sub_array[] = $row['designation_name'];
+    $sub_array[] = $row['year'];
+    $status    = $row['status'];
+    
+    
+   
+    if($status == 1)
+	{
+	$sub_array[] = "<span style='width: 144px;'><span class='kt-badge  kt-badge--danger kt-badge--inline kt-badge--pill'>Inactive</span></span>";
+	}
+	else
+	{
+    $sub_array[] = "<span style='width: 144px;'><span class='kt-badge  kt-badge--success kt-badge--inline kt-badge--pill'>Active</span></span>";
+	}
+	$id   = $row['goal_setting_id'];
+	
+	$action="<a href='goal_setting&upd=$id' title='Edit details'><span class='icon-border_color'></span></a>&nbsp;&nbsp; 
+	<a href='goal_setting&del=$id' title='Delete details' class='delete_goal_setting'><span class='icon-trash-2'></span></a>";
+
+	$sub_array[] = $action;
+    $data[]      = $sub_array;
+    $sno = $sno + 1;
+  
+}
+
+function count_all_data($connect)
+{
+    $query     = "SELECT gs.company_name,c.company_name,gs.department,dc.department_name,gs.role,ds.designation_name,gs.year,y.year,gs.status FROM goal_setting gs 
+    LEFT JOIN goal_setting_ref gsr ON gsr.goal_setting_id=gs.goal_setting_id 
+    LEFT JOIN company_creation c ON c.company_id=gs.company_name 
+    LEFT JOIN department_creation dc ON dc.department_id=gs.department 
+    LEFT JOIN designation_creation ds ON ds.designation_id=gs.role 
+    LEFT JOIN year_creation y ON y.year_id=gs.year";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+    return $statement->rowCount();
+}
+
+
+
+
+$output = array(
+    'draw' => intval($_POST['draw']),
+    'recordsTotal' => count_all_data($connect),
+    'recordsFiltered' => $number_filter_row,
+    'data' => $data
+    // 'deptname' => $deptname
+);
+
+
+echo json_encode($output);
+?>
+
+
+
+
+
--- js/goal_setting.js
+++ js/goal_setting.js
@@ -1,4 +1,13 @@
 // Document is ready
+$(function(){
+    var idupd = $('#id').val();
+    if(parseInt(idupd) > 0){
+        var company_id_upd = $('#company_id_upd').val();
+        insertData(company_id_upd);
+        
+    }
+})
+
 $(document).ready(function () {
 //    $('#syear').picker();
     // remove delete option for last child
@@ -8,6 +17,37 @@ $(document).ready(function () {
     // if($('#audit_area_id').val() > 0){
     //     $('#date_of_audit').attr("disabled", true);
     // }
+
+
+
+    $(document).on("click", '#yes', function () {
+        var opt =$('input:visible:checked').val();
+        if(opt == 'Yes'){
+            var year = new Date().getFullYear();
+           var prevyear = parseInt(year) - parseInt(1);
+
+            // console.log('year',year);
+            // console.log('prevyear',prevyear);
+            $.ajax({
+                url: 'ajaxGetyeardetailes.php',
+                data: {"prevyear": prevyear},
+                cache: false,
+                type: "post",
+                dataType: "json",
+                success: function (data) {
+    
+                    
+                }
+            });
+        }else{ console.log('clicked'); }
+        
+
+        
+    
+    });
+    
+
+
 
     $("#date_of_audit").change( function(){
 
@@ -49,7 +89,7 @@ $(document).ready(function () {
          "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" +
          "<td> <span class='icon-trash-2' tabindex='10' id='delete_row'></span></td></tr>";
         $('#moduleTable').find('tbody').append(appendTxt);
-        sts();
+        // sts();
     }));
 //  <input tabindex='7' type='text' class='form-control' id='Astatus' name='Astatus[]'>
     // Delete unwanted Rows
@@ -71,42 +111,6 @@ $(document).ready(function () {
         $('#designation').val('');
 
     }
-
-    // insert Data into Module Table
-    function insertData(prev_company){
-        $('#moduleTable').find('tbody').empty();
-
-                    $.ajax({
-                        url: 'getgoalsettings.php',
-                        data: {'prev_company': prev_company },
-                        cache: false,
-                        type:'post',
-                        dataType: 'json',
-                        success: function(data){
-                            $('#dept').text('');
-                            $('#designation').text('');
-                            $('#dept').val('');
-                            $('#designation').val('');
-                            var option4 = $('<option></option>').val('').text('Select Department');
-                            $('#dept').append(option4);
-                            var option5 = $('<option></option>').val('').text('Select Role');
-                            $('#designation').append(option5);
-                        
-                            for(var a=0; a<=data.length-1; a++){
-                           
-                             var option1 = $('<option></option>').val(data[a]['department_id']).text(data[a]['department_name']);
-                             $('#dept').append(option1);
-
-                             var option2 = $('<option></option>').val(data[a]['designation_id']).text(data[a]['designation_name']);
-                             $('#designation').append(option2);
-                            }
-                              // $('#delete_row:last').filter(':last').attr('id', '');
-                        }
-                      });
-                      getyear();
-    }
-
-
 
     // insert year <a href="admin.php?function=myFunction">Click here to call myFunction</a>
     $(document).on("click", '#insert', function () {
@@ -223,18 +227,80 @@ getyear();
 
  function getyear(){
     var insertedcompany = $('#prev').val();
+    var year_idup =$('#year_idup').val();
     $.ajax({
         url: 'fetchyear.php',
         data: {'insertedcompany': insertedcompany},
         type: "post",
         dataType: "json",
-        success: function (data) { 
-            console.log(data);
-            for(var a=0; a<=data.length-1; a++){
-            var option = $('<option></option>').val(data[a]['year_id']).text(data[a]['year']);
+        success: function (data) {
+            
+           
+          
+              $('#syear').text('');
+            $('#syear').val('');
+            var option = $('<option></option>').val('').text('Select Year');
             $('#syear').append(option);
-        }
+
+
+            for(var a=0; a<=data.length-1; a++){
+                var selected = '';
+                if(year_idup == data[a]['year_id']){
+                    selected = 'selected';
+                }
+
+            var option = $('<option '+selected+' ></option>').val(data[a]['year_id']).text(data[a]['year']);
+            $('#syear').append(option);
+            }
+       
         }
     });
 
 }
+
+// insert Data into Module Table
+function insertData(prev_company){
+    // $('#moduleTable').find('tbody').empty();
+    var dept_id_upd = $('#dept_id_upd').val();
+    var role_id_up = $('#role_id_up').val();
+    $.ajax({
+        url: 'getgoalsettings.php',
+        data: {'prev_company': prev_company },
+        cache: false,
+        type:'post',
+        dataType: 'json',
+        success: function(data){
+            $('#dept').text('');
+            $('#designation').text('');
+            $('#dept').val('');
+            $('#designation').val('');
+            var option4 = $('<option></option>').val('').text('Select Department');
+            $('#dept').append(option4);
+            var option5 = $('<option></option>').val('').text('Select Role');
+            $('#designation').append(option5);
+        
+            for(var a=0; a<=data.length-1; a++){
+                
+                var selected = '';
+                if(dept_id_upd == data[a]['department_id']){
+                    selected = 'selected';
+                }
+                if(role_id_up == data[a]['designation_id']){
+                    selected = 'selected';
+                }
+                var option1 = $('<option '+selected+'></option>').val(data[a]['department_id']).text(data[a]['department_name']);
+                $('#dept').append(option1);
+
+                var option2 = $('<option '+selected+' ></option>').val(data[a]['designation_id']).text(data[a]['designation_name']);
+                $('#designation').append(option2);
+            }
+            // var appendTxt = "<tr><td><input tabindex='4' type='text' class='form-control' id='assertion' placeholder='Enter Assertion' name='assertion[]'></td>"+
+            // "<td><input tabindex='6' type='text' class='form-control' id='target' name='target[]' placeholder='Enter Target'></td>"+
+            // "<td> <button type='button' tabindex='9' id='add_row' name='add_row' value='Submit' class='btn btn-primary add_row'>Add</button></td>" +
+            // "<td> <span class='icon-trash-2' tabindex='10' id='delete_row'></span></td></tr>";
+            // $('#moduleTable').find('tbody').append(appendTxt);
+        }
+        });
+        getyear();
+}
+
