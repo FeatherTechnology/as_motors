5ff8deacc2315d15683a185a1aeab21e8cc05d20
Rabi-Jerome <rabijerome.p@gmail.com>
2023-09-04 11:53:49 +0530

    update work in basic creation
    1. in basic creation main branch manager or reporting to director are also list out with branch designation in reporting to dropdown - Completed.

--- ajaxResetReportingDropdown.php
+++ ajaxResetReportingDropdown.php
@@ -1,4 +1,6 @@
 <?php 
+// 4 - Director....the employee can be in any company or branch but they can report to main branch manager or the manager who reporting to MD. so we assign director designation id static, if director designation change here also change it.
+
 include('ajaxconfig.php');
 
 if (isset($_POST['branch_id'])) {
@@ -6,14 +8,18 @@ if (isset($_POST['branch_id'])) {
 }
 
 $reportingArr = array();
+$designation_id1 = array();
 $result=$con->query("SELECT * FROM basic_creation where company_id='".$company_id."' and status=0");
 while( $row = $result->fetch_assoc()){
     $designation_id1[] = $row['designation'];
 }
 
 $reportingStr = implode(",", $designation_id1);
-$reporting = array_map('intval', explode(',', $reportingStr)); 
-
+if($reportingStr !=''){
+    $reporting = array_map('intval', explode(',', $reportingStr)); 
+}else{
+    $reporting = array();
+}
 // remove array duplicates without affect array index
 $designation_id=$reporting;
 $duplicated=array();
@@ -27,6 +33,7 @@ if( ($kt=array_search($v,$designation_id))!==false and $k!=$kt )
 sort($designation_id); // optional
 // end here
 
+$designation_name = array();
 for($i=0;$i<=sizeof($designation_id)-1;$i++){
     $result1=$con->query("SELECT * FROM designation_creation where designation_id='".$designation_id[$i]."' and status=0");
     while( $row1 = $result1->fetch_assoc()){
@@ -36,6 +43,40 @@ for($i=0;$i<=sizeof($designation_id)-1;$i++){
 
 $reportingArr['designation_id'] = $designation_id;
 $reportingArr['designation_name'] = $designation_name;
+
+
+$managerLevelQry = $con->query("SELECT * FROM basic_creation where report_to = '4' and company_id !='".$company_id."' and status=0"); // 4 - Director....the employee can be in any company or branch but they can report to main branch manager or the manager who reporting to MD. so we assign director designation id static, if director designation change here also change it.  
+$design_id = array();
+while( $managerLevelInfo = $managerLevelQry->fetch_assoc()){
+    $design_id[] = $managerLevelInfo['designation'];
+}
+
+$reportStr = implode(",", $design_id);
+$report = array_map('intval', explode(',', $reportStr)); 
+
+// remove array duplicates without affect array index
+$desgn_id=$report;
+$duplicat=array();
+
+foreach($desgn_id as $k=>$v) {
+
+if( ($kt=array_search($v,$desgn_id))!==false and $k!=$kt )
+{ unset($desgn_id[$kt]);  $duplicat[]=$v; }
+
+}
+sort($desgn_id); // optional
+// end here
+
+for($i=0;$i<=sizeof($desgn_id)-1;$i++){
+    $result1=$con->query("SELECT * FROM designation_creation where designation_id='".$desgn_id[$i]."' and status=0");
+    if(mysqli_num_rows($result1) > 0 ){
+        $row1 = $result1->fetch_assoc();
+        array_push($reportingArr['designation_id'], $desgn_id[$i]);
+        array_push($reportingArr['designation_name'], $row1['designation_name']);
+    }
+
+}
+
 
 echo json_encode($reportingArr);
 ?>
