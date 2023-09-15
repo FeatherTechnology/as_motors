c1b6bffd509d34b5b7752e8de4ec32126c96f94b
Rabi-Jerome <rabijerome.p@gmail.com>
2023-09-15 17:47:05 +0530

    chnages in dashboard
    after staff transfer till the effective date the previous data has to maintain like branch, designation, etc.,

--- ajaxAuditeeResponseFetchDashboard.php
+++ ajaxAuditeeResponseFetchDashboard.php
@@ -13,6 +13,25 @@ if(isset($_SESSION["designation_id"])){
 }else{
     $designation_id = 0;
 }
+if(isset($_SESSION["staffid"])){
+    $staffid = $_SESSION["staffid"];
+}else{
+    $staffid = 0;
+}
+
+//if the staff is transfered then check the transfer effective date is greater than curdate if true then take old designation from the staff_creation_history, if false means the designation will not be overwrite 
+$getdesgnDetails = $con->query("SELECT tl.transfer_effective_from, sch.designation FROM `transfer_location` tl LEFT JOIN staff_creation_history sch ON tl.transfer_location_id = sch.transfer_location_id WHERE tl.staff_code = '$staffid' order by tl.transfer_location_id DESC LIMIT 1");
+        
+if(mysqli_num_rows($getdesgnDetails)>0){
+    $dsgnInfo = $getdesgnDetails->fetch_assoc();
+    $transfer_effective_from = date('Y-m-d',strtotime($dsgnInfo['transfer_effective_from'])); 
+    $curdates = date('Y-m-d');
+
+    if($transfer_effective_from > $curdates){
+        $designation_id = $dsgnInfo['designation']; //Old Designation.
+        
+    }
+}
 
 $column = array(
 
