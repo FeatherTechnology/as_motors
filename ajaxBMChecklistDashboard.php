a62bbb0f6b1e80ba25a5ec16ed1c768395d84292
Rabi-Jerome <rabijerome.p@gmail.com>
2024-02-06 17:07:24 +0530

    changes on dashboard files
    1. close connection - in server max connection exceeded.
    2. pending task - krakpi map table query changed.

--- ajaxBMChecklistDashboard.php
+++ ajaxBMChecklistDashboard.php
@@ -163,5 +163,6 @@ $output = array(
 echo json_encode($output);
 
 // Close the database connection
+$mysqli->close();
 $connect = null;
 ?>
--- ajaxPMChecklistDashboard.php
+++ ajaxPMChecklistDashboard.php
@@ -165,5 +165,6 @@ $output = array(
 echo json_encode($output);
 
 // Close the database connection
+$mysqli->close();
 $connect = null;
 ?>
